5ae09e7d5123bc9b803fd6f4ca6b50425513d236
Emma66886 <52328102+Emma66886@users.noreply.github.com>
2022-08-01 14:19:52 +0100

    updatemenu

--- app.js
+++ app.js
@@ -1,21 +1,47 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "vertical",
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
+// onload((event)=>{
+  let menuOverLay = document.getElementById('overlay');
+  let leftSideBar = document.getElementById('left-side');
+    var mobileMenuOpen = document.getElementById('checkbtn');
+    // console.log(mobileMenuOpen)
+  mobileMenuOpen.onclick=(event)=>{
+    console.log("I am clicked")
+    leftSideBar.classList.remove('hide')
+    menuOverLay.classList.remove('hide')
+    mobileMenuOpen.classList.remove('show')
+    leftSideBar.classList.add(['show'])
+    menuOverLay.classList.add(['show'])
+    mobileMenuOpen.classList.add(['hide'])
+  }
+  menuOverLay.onclick=(event)=>{
+    console.log("I am clicked")
+    leftSideBar.classList.remove('show')
+    menuOverLay.classList.remove('show')
+    mobileMenuOpen.classList.remove('hide')
+    leftSideBar.classList.add(['hide'])
+    menuOverLay.classList.add(['hide'])
+    mobileMenuOpen.classList.add(['show'])
+  }
+// })
+
+
